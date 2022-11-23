eeb087e8d9072eba45a4cbb71f3888e926d7ad08
never2707 <117578247+never2707@users.noreply.github.com>
2022-11-23 08:51:10 +0800

    Add files via upload

--- linebot 程式.py
+++ linebot 程式.py
@@ -44,7 +44,10 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+    if re.match('黑人',message):
+        line_bot_api.reply_message(event.reply_token,TextSendMessage('nigger'))
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
 #主程式
 import os
