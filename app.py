2c0ca8248dbab47b1f0c5f2ca2caf419004ce1c3
never2707 <117578247+never2707@users.noreply.github.com>
2022-12-21 09:54:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -46,12 +46,11 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print("Handle: reply_token: " + event.reply_token + ", message: " + event.message.text)
-    content = "{}: {}".format(event.source.user_id, event.message.text)
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=content))
-
+    message = TextSendMessage(text=event.message.text)
+    if re.match('黑人',message):
+        line_bot_api.reply_message(event.reply_token,TextSendMessage('nigger'))
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
 #主程式
 import os
