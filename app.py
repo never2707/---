f9e4aed0bc00e23288d3fd816e9cf421f1577ef4
never2707 <117578247+never2707@users.noreply.github.com>
2022-12-21 10:15:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -41,16 +41,27 @@ def callback():
 
     return 'OK'
 
- 
+
+
 #訊息傳遞區塊
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    if re.match('黑人',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('nigger'))
-    else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+    line_bot_api.reply_message(event.reply_token,message)
+
+
+
+
+#訊息傳遞區塊
+##### 基本上程式編輯都在這個function #####
+#@handler.add(MessageEvent, message=TextMessage)
+##def handle_message(event):
+ #   message = TextSendMessage(text=event.message.text)
+  #  if re.match('黑人',message):
+   #     line_bot_api.reply_message(event.reply_token,TextSendMessage('nigger'))
+#    else:
+ #       line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
 #主程式
 import os
