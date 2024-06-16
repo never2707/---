0f5651de2ff4c69abc24c1884024e3b4681b2665
never2707 <117578247+never2707@users.noreply.github.com>
2024-06-16 21:55:34 +0800

    Update Setter.java

--- Game_minesweeper/Setter.java
+++ Game_minesweeper/Setter.java
@@ -1,7 +1,7 @@
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
-import java.awt.GridLayout;
+import java.awt.Font;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -9,18 +9,19 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+
 public class Setter extends JFrame implements MouseListener{
 
     public static void main(String[] args) {
         Setter s = new Setter();
     }
 
-    int mode;    
+    int mode = 0;    
 
     public Setter(){
         
         super("choose mode");
-        setSize(300, 250); 
+        setSize(240,120); 
 		setResizable(false); //設定大小不可調整。
 		setDefaultCloseOperation(EXIT_ON_CLOSE); //設定關閉按鈕。
 		setLocationRelativeTo(this); //顯示為置中。
@@ -30,7 +31,7 @@ public class Setter extends JFrame implements MouseListener{
         
         JPanel panel = new JPanel();
         panel.setFont(getFont());
-        panel.setLayout(new GridLayout(2,3));
+        panel.setLayout(null);
         JButton easy = new JButton("easy");
         JButton normal = new JButton("normal");
         JButton hard = new JButton("hard");
@@ -43,13 +44,16 @@ public class Setter extends JFrame implements MouseListener{
 
         for(int i=0; i<3; i++) {
             buttons[i].setBackground(Color.WHITE);
-            buttons[i].setBounds(i, i, 30, 20);
+            buttons[i].setBounds(0+i*80, 0, 80, 40);
             buttons[i].addMouseListener(this);
             panel.add(buttons[i]);
         }
         JButton confirm = new JButton("確定");
         confirm.setActionCommand("Y");
         confirm.addMouseListener(this);
+        Font font = new Font(Font.DIALOG, Font.BOLD, 5);
+        confirm.setFont(getFont());
+        confirm.setBounds(90, 45, 60, 30);
         panel.add(confirm);
         confirm.setBackground(Color.WHITE);
         add(panel, "Center");
@@ -116,3 +120,4 @@ public class Setter extends JFrame implements MouseListener{
 
     }
 }
+
